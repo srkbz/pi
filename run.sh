ef6a36d9ce0170f64305f59715efd5817a7f1248
Carlos Fdez. Llamas <hello@sirikon.me>
2024-08-08 15:57:50 +0200

    wip

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+TASK_VERSION="3.38.0"
+
+function main {
+    ensure-task
+    "$(task-path)" "$@"
+}
+
+function ensure-task {
+    if [ ! -f "$(task-path)" ]; then
+        sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b "$(dirname "$(task-path)")" "v${TASK_VERSION}"
+    fi
+}
+
+function task-path {
+    printf "%s" ".cache/task/${TASK_VERSION}/task"
+}
+
+main "$@"
